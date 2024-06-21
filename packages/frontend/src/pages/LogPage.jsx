214ffbd40b19de6babf68b2048075f798dc57ce0
laurascotelari <laura.scotelari@usp.br>
2024-06-21 16:20:38 -0300

    Apagando comentarios desnecessarios

--- packages/frontend/src/pages/LogPage.jsx
+++ packages/frontend/src/pages/LogPage.jsx
@@ -3,18 +3,6 @@ import './LogPage.css';
 import { useNavigate } from "react-router-dom";
 
 export default function LogPage() {
-    /*const logList = [
-        { logLine: "03/22 08:51:01 INFO   : Venda efetuada 15 Cloridrato de Metilfenidato | EMS" },
-        { logLine: "03/23 09:20:15 INFO   : Venda efetuada 20 Ibuprofeno | Medley" },
-        { logLine: "03/24 10:35:42 INFO   : Venda efetuada 5 Paracetamol | Neo Química" },
-        { logLine: "03/25 11:10:05 INFO   : Venda efetuada 8 Omeprazol | Sandoz" },
-        { logLine: "03/26 12:55:30 INFO   : Venda efetuada 12 Loratadina | Aché" },
-        { logLine: "03/22 08:51:01 INFO   : Venda efetuada 13 Cloridrato de Metilfenidato | EMS" },
-        { logLine: "03/23 09:22:05 INFO   : Venda efetuada 10 Cloridrato de Paracetamol | Medley" },
-        { logLine: "03/24 10:33:10 INFO   : Venda efetuada 5  Ibuprofeno | Bayer" },
-        { logLine: "03/25 11:44:15 INFO   : Venda efetuada 20 Dipirona | Novartis" },
-        { logLine: "03/26 12:55:20 INFO   : Venda efetuada 27 Omeprazol | EMS" },
-    ];*/
 
     const [logList, setLogList] = useState([]);
       
